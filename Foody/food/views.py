2a3d94072c1d176542854019ddd8ee086bd8b78f
Kaverya Vladimir <vova.kaverya@mail.ru>
2023-07-07 17:43:49 +0300

    init

---
+++ Foody/food/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main (request):
+    context = {
+        'title': 'Foody',
+    }
+    return render(request, 'food/main.html', context=context)
